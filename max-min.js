7859a78f20bf10507c7af998e72685e776b621ab
Scott Dowding <scottddowding1279@gmail.com>
2019-02-20 14:25:46 -0500

    added repeat function to min-max.js

--- max-min.js
+++ max-min.js
@@ -37,3 +37,20 @@ function average(arr) {
   });
   return total / arr.length;
 }
+
+function repeat(fn, n) {
+    for (let i=0; i < n; i++) {
+        fn();
+    }    
+}
+
+function hello() {
+    console.log('Hello world');
+}
+
+function goodbye() {
+    console.log('Goodbye world');
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
