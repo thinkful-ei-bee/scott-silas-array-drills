5fd6805e631cd6e79ba56274d00e7ee76e5bf329
Silas <silasishallahan@gmail.com>
2019-02-20 11:40:31 -0800

    added filter function

--- max-min.js
+++ max-min.js
@@ -1,33 +1,33 @@
 'use strict';
 
 function max(numbers) {
-    if (typeof numbers !== 'object') {
-        return undefined;
+  if (typeof numbers !== 'object') {
+    return undefined;
+  }
+  let max = numbers[0];
+  let i = 1;
+  while (i < numbers.length) {
+    if (numbers[i] > max) {
+      max = numbers[i];
     }
-    let max = numbers[0];
-    let i = 1;
-    while (i < numbers.length) {
-        if (numbers[i] > max) {
-            max = numbers[i];
-        }
-        i++;
-    }
-    return max;
+    i++;
+  }
+  return max;
 }
 
 function min(numbers) {
-    if (typeof numbers !== 'object') {
-        return undefined;
+  if (typeof numbers !== 'object') {
+    return undefined;
+  }
+  let min = numbers[0];
+  let i = 1;
+  while (i < numbers.length) {
+    if (numbers[i] < min) {
+      min = numbers[i];
     }
-    let min = numbers[0];
-    let i = 1;
-    while (i < numbers.length) {
-        if (numbers[i] < min) {
-            min = numbers[i];
-        }
-        i++;
-    }
-    return min;
+    i++;
+  }
+  return min;
 }
 
 function average(arr) {
@@ -39,18 +39,26 @@ function average(arr) {
 }
 
 function repeat(fn, n) {
-    for (let i=0; i < n; i++) {
-        fn();
-    }    
+  for (let i=0; i < n; i++) {
+    fn();
+  }    
 }
 
 function hello() {
-    console.log('Hello world');
+  console.log('Hello world');
 }
 
 function goodbye() {
-    console.log('Goodbye world');
+  console.log('Goodbye world');
 }
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+function filter(arr, fn) {
+  // TASK: Define your function here
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])){
+      newArray.push(arr[i]);
+    }
+  } return newArray;
+}
+console.log(filter(['Rich', 'Joe', 'Bhaumik', 'Ray'], (name) => name[0] === 'R'));
